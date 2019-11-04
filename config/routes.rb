76794c20538f3f7e050aa4962966086424da1ffd
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-04 16:40:45 +0000

    creating user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
-  root 'home#index'
-
-  get 'user/index'
 
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :posts
+
+  root 'home#index'
 end
