0aa547b7d27ec80ea19e9a0ea9d22e8c3e9d3d3e
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-07 14:19:38 +0000

    private messaging tables created

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,10 @@
+class ConversationsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+end
---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,10 @@
+class MessagesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+end
