69ef47cbb4833a8d19a1429b3e700895a95d2f42
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-14 16:44:09 +0000

    Implements comment likes that do not require page refresh

--- spec/feature_helpers.rb
+++ spec/feature_helpers.rb
@@ -11,7 +11,7 @@ def signup_as_new_user(user)
   fill_in "user[email]", with: "#{user}@lizard.com"
   fill_in "user[full_name]", with: "Dr #{user} Lizard"
   select "Gecko", :from => "user[lizard_species]"
-  fill_in "user[birthday]", with: "01/01/2019"
+  fill_in "user[birthday]", with: "2010-01-01"
   fill_in "user[profile_picture]", with: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
   click_button "Create Account"
   click_link "LogOut"
@@ -21,4 +21,18 @@ def login_as_user(user)
   fill_in "email", with: "#{user}@lizard.com"
   fill_in "Password", with: "password"
   click_button "Login"
+end
+
+def signup_as_new_user_js(user)
+  visit "/"
+  click_button "Create Account"
+  fill_in "user[username]", with: "#{user}-Lizard"
+  fill_in "user[password]", with: "password"
+  fill_in "user[email]", with: "#{user}@lizard.com"
+  fill_in "user[full_name]", with: "Dr #{user} Lizard"
+  select "Gecko", :from => "user[lizard_species]"
+  fill_in "user[birthday]", with: "2010-01-01"
+  fill_in "user[profile_picture]", with: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+  click_button "Create Account"
+  click_link "LogOut"
 end
